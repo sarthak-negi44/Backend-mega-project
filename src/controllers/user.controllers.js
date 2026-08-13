d5229e85dd710de2c32204feb14e55868c1509e2
Sarthak Negi <sarthakn658@gmail.com>
2026-08-13 06:56:28 -0700

    adding data to mongodb

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,5 +1,14 @@
 import asyncHandler from "../utils/asyncHandler.js"
 const registerUser = asyncHandler(async (req, res) =>{
+    // get user data form frontend
+    // validation - not empty, email format, password length
+    // check if user already exists in db: with email and username
+    // check for images and avatar
+    // upload images to cloudinary
+    // creqte user object - create entry in db
+    // remove password and refreshTokens from user object before sending response
+    // check for the user creation
+    // return response to frontend with user object and success message
 res.status(200).json({
     message: "ok"
 })
